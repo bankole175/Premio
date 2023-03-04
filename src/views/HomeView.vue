78267198c44fe773c1bcfb9dfa4ca372b218f4a9
Samuel Bankole <bankolesamuel175@gmail.com>
2023-03-04 10:35:44 +0100

    chore: initial commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,23 @@
+<template>
+  <main>
+    <h1>Favorite Foods</h1>
+    <draggable v-model="meals" tag="ul">
+      <template #item="{ element: meal }">
+        <li>{{ meal }}</li>
+      </template>
+    </draggable>
+  </main>
+</template>
+
+<script setup lang="ts">
+import draggable from 'vuedraggable'
+import { ref } from 'vue'
+
+const meals = ref([
+  'Hamburger',
+  'Pizza',
+  'Spaghetti',
+  'Tacos',
+  'Teriyaki Chicken'
+])
+</script>
